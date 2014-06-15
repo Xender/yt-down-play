c8ac91d12f8746c31efd220997042d1f7bb1c75d
Aleksander Nitecki <ixendr@itogi.re>
2014-06-15 20:14:46 +0200

    Extract player spawning for readability

--- yt.py
+++ yt.py
@@ -7,6 +7,15 @@ player_cmd = ['mpv', '--keep-open']
 
 youtube_dl_destination_filename_msg_prefix = '[download] Destination: '
 
+def spawn_player(filename):
+	return subprocess.Popen(
+		player_cmd + ['--',
+			filename
+		],
+		stdin = subprocess.DEVNULL
+	)
+	# TODO Should start_new_session=True be also used above?
+
 def main(argv):
 	youtube_dl = subprocess.Popen(
 		['youtube-dl'] + argv[1:],
@@ -25,12 +34,7 @@ def main(argv):
 					video_filename = line[len(youtube_dl_destination_filename_msg_prefix) : -1] # -1 for trailing '\n'.
 					video_filename_already_found = True
 
-					subprocess.Popen(
-						player_cmd + ['--',
-							video_filename+'.part'
-						],
-						stdin = subprocess.DEVNULL)
-						# TODO Should start_new_session=True be also used above?
+					spawn_player( video_filename+'.part' )
 
 if __name__ == "__main__":
 	main(sys.argv)
