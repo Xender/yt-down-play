178d088857ba09d039bda1b00e73b62f29203bea
Aleksander Nitecki <ixendr@itogi.re>
2014-06-17 00:56:22 +0200

    Header buffering for mpv \o/

--- yt.py
+++ yt.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 
 import sys
+import os
 import subprocess
 
 player_cmd = ['mpv', '--keep-open']
+# --keep-open handles unbuffered video,
+# but when mpv cannot detect a valid header
+# it exeits even with that option.
+# Care of that is taken later.
 
 youtube_dl_destination_filename_msg_prefix = '[download] Destination: '
 
@@ -24,6 +29,7 @@ def main(argv):
 	) as youtube_dl:
 
 		video_filename_already_found = False
+		headers_already_buffered     = False
 
 		for line in youtube_dl.stdout:
 			sys.stdout.write(line)
@@ -33,10 +39,15 @@ def main(argv):
 			# (This script is not intended for that anyway, but better safe than sorry.)
 			if not video_filename_already_found:
 				if line.startswith(youtube_dl_destination_filename_msg_prefix):
-					video_filename = line[len(youtube_dl_destination_filename_msg_prefix) : -1] # -1 for trailing '\n'.
+					video_filename = line[len(youtube_dl_destination_filename_msg_prefix) : -1]+'.part' # -1 for trailing '\n'.
 					video_filename_already_found = True
 
-					spawn_player( video_filename+'.part' )
+			else: # == Video filename already found (double negative).
+				if not headers_already_buffered:
+					if os.path.getsize(video_filename) >= 64*1024: # Arbitrary headers size guess. Now testing 64k
+						headers_already_buffered = True
+						spawn_player(video_filename)
+
 
 if __name__ == "__main__":
 	main(sys.argv)
